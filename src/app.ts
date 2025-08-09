f149c1814c540b50ff5f8d58becb665044c77737
BINIL <binilbalan.e.k@gmail.com>
2025-08-09 21:32:32 +0530

    Add user controller and route; update package.json for Swagger support

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-// import routes from '@routes/index';
+import router from '@routes/index';
 const app = express();
-
 app.use(cors())
 app.use(express.json());
-
+app.use("/app",router)
 export default app;
 
 
---
+++ src/routes/user/user.route.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+import { createUser } from '@controllers/user/user.controller';
+const userRouter = express.Router();
+userRouter.get("/",createUser);
+
+export default userRouter
