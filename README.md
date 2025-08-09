5ea13a3cc43b336eeb47633fc30cc826c8287192
BINIL <binilbalan.e.k@gmail.com>
2025-08-09 22:14:34 +0530

    Add Swagger documentation setup and update README; modify package.json scripts

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Card game
+
+A brief description of what this project does and who it's for
+
+## Documentation
+
+[Documentation](http://localhost:8000/api-docs)
+
+
+## 🚀 About Me
+I'm a full stack developer...
+
+
+## 🔗 Links
+[![GitHub link](https://github.com/BINIL-BALAN/card_game_server.git)](https://github.com)
+
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import router from '@routes/index';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from '@config/swagger';
 const app = express();
 app.use(cors())
 app.use(express.json());
 app.use("/app",router)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 export default app;
 
 
