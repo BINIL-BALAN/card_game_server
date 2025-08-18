d1a8cc7d5bfe058b1797b3be97006863902f8e4d
BINIL <binilbalan.e.k@gmail.com>
2025-08-18 21:38:37 +0530

    security dependency installed

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,21 @@ import router from '@routes/index';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from '@config/swagger.config';
 import { responseMiddleware } from '@middlewares/globalResponse/globalResponse.middleware';
-import httpLogger from '@middlewares/logger/logger.middleware';
+import cookieParser from "cookie-parser";
 import globalErrorHandling from '@middlewares/errorHandling/globalerror.middleware';
+import googleAuth from "@config/googleAuth/auth.json"
+// import httpLogger from '@middlewares/logger/logger.middleware';
+console.log(googleAuth)
 const app = express();
 app.use(cors())
 app.use(express.json());
+app.use(cookieParser());
 app.use(responseMiddleware)
 // app.use(httpLogger)
 app.use("/app",router)
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use(globalErrorHandling)
 export default app;
+
 
 
